fee0e85ad93f63dd5dd1b807afcc325d1b4d0e0d
Arnold N. Tharrington <arnoldt@ornl.gov>
2019-02-14 15:00:26 -0500

    Initial commit of icmake install file.

---
+++ bin/install_icmake.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env bash
+
+#-----------------------------------------------------
+# Define the project top level directory.            -
+#                                                    -
+#-----------------------------------------------------
+export PROJECT_TOP_LEVEL=${INSPIRE_PROJECT_TOP_LEVEL}
+
+#-----------------------------------------------------
+# Define the directory path to the top level         -
+# CMakeLists.txt file                                -
+#                                                    -
+#-----------------------------------------------------
+export PROJECT_TOP_LEVEL_ICMAKE=${INSPIRE_PROJECT_TOP_LEVEL}/sources/icmake/icmake
+
+#-----------------------------------------------------
+# Define the install location of icMake.             -
+#                                                    -
+#-----------------------------------------------------
+export MY_INSTALL_PREFIX=${INSPIRE_PROJECT_TOP_LEVEL}/sw/summit/icmake
+
+#-----------------------------------------------------
+# Change to the build directory and build cmake.     -
+#                                                    -
+#-----------------------------------------------------
+cd ${PROJECT_TOP_LEVEL_ICMAKE}
+
+./icm_prepare /
+./icm_bootstrap [strip] x
+./icm_install all ${MY_INSTALL_PREFIX}
+rm -rf tmp
